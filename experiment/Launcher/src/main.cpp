1653fa16540f4ab958d4b89dfe653dd363a309a5
Jokke Jansen <jokke.j@posteo.de>
2021-09-06 12:50:34 +0200

    revert

--- experiment/Launcher/src/main.cpp
+++ experiment/Launcher/src/main.cpp
@@ -64,10 +64,9 @@ void checkRC(int rc) {
 	};
 }
 
-string printConfig(string scenarioName, unsigned numSims, double expConst, double discount) {
+void printConfig(string scenarioName, unsigned numSims, double expConst, double discount) {
         string config = scenarioName + ": " + std::to_string(numSims) + " Simulations, " + std::to_string(expConst) + " exploration constant, " + std::to_string(discount) + " discount";
         std::cout << config  << std::endl;
-        return config;
 }
 
 void readConfig(CSimpleIniA& ini) {
@@ -91,7 +90,7 @@ void writeConfig(CSimpleIniA& ini, unsigned numSims, double expConst, double dis
 	checkRC(rc);
 }
 
-void launch(string& config) {
+void launch() {
 	string command = "screen -d -m ";
 	command += "torcs -L /home/jokke/Repositories/TorcsPOMDP/build/lib/torcs -D /home/jokke/Repositories/TorcsPOMDP/build/share/games/torcs -r /home/jokke/Repositories/TorcsPOMDP/build/share/games/torcs/config/raceman/quickrace.xml -d &";
 	system(command.c_str());
@@ -116,9 +115,9 @@ int main(int argc, char *argv[])
 		unsigned numSims = NUM_SIMS_SCENARIOS[numSimsScenarioIdx];
 		double expConst = EXP_CONST_SCENARIOS[expConstScenarioIdx];
 		double discount = DISCOUNT_SCENARIOS[discountScenarioIdx];
-		string config = printConfig(scenarioName, numSims, expConst, discount);
+		printConfig(scenarioName, numSims, expConst, discount);
 		writeConfig(ini, numSims, expConst, discount);
-		launch(config);
+		launch();
 	}
 
 	return 0;
